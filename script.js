ce652c507d24b5a1107197733e9130d3c83fa641
HemantS1609 <hemantsonar1609@gmail.com>
2025-04-16 13:20:20 +0530

    init commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const text = "Frontend Developer";
+let index = 0;
+const typingText = document.getElementById("typing-text");
+
+function type() {
+  if (index < text.length) {
+    typingText.textContent += text.charAt(index);
+    index++;
+    setTimeout(type, 150);
+  } else {
+    setTimeout(erase, 1000);
+  }
+}
+
+function erase() {
+  if (index > 0) {
+    typingText.textContent = typingText.textContent.slice(0, index - 1);
+    index--;
+    setTimeout(erase, 100);
+  } else {
+    setTimeout(type, 1000);
+  }
+}
+
+type();
+
+const date = document.getElementById("date");
+date.textContent += new Date().getFullYear();
